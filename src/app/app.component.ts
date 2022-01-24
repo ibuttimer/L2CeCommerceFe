f4aa434b9c73c4c90701132818f3c485e3329eb9
Ian Buttimer <ibuttimer@yahoo.com>
2022-01-24 16:07:00 +0000

    added hello service to improve host responsiveness when running on heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { environment } from '../environments/environment';
+import { HelloService } from './services/hello.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'l2cecommercefe';
 
-  constructor() {
+  constructor(private helloService: HelloService) {
 
     console.log('host %s  production %o', environment.apiUrl, environment.production);
+  }
+
+  ngOnInit(): void {
+    // send hello to improve host responsiveness when running on heroku
+    this.helloService.sayHello().subscribe((message: String) => {
+      console.log('host response: %s', message);
+    });
   }
 }
---
+++ src/app/services/hello.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelloService extends BaseService {
+
+  private baseUrl = `${this.apiUrl}/hello`
+
+  constructor(protected httpClient: HttpClient) {
+    super(httpClient);
+  }
+
+  /**
+   * Say hello to server
+   * @return {Observable<Object>} 
+   */
+   sayHello(): Observable<String> {
+    return this.httpClient.get<GetRespHello>(this.baseUrl).pipe(
+      map(response => response.message)
+    );
+  }
+}
+
+/**
+ * Interface to unwrap response
+ */
+ export interface GetRespHello {
+  message: String;
+}
+
