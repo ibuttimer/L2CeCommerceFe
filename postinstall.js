4e8b7fbb18bc9cb82ad29feee32dc0ebe50c011d
Ian Buttimer <ibuttimer@yahoo.com>
2023-07-06 14:17:34 +0100

    chore: add postinstall script logging

--- postinstall.js
+++ postinstall.js
@@ -10,6 +10,7 @@ const commands = [
 ]
 
 for (let cmd of commands) {
+  console.log(`postinstall: ${cmd}`);
   exec(cmd, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
